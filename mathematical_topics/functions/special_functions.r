d83fe3a37bb0ab83b841040c93d469dae6850366
gprzy <gabrielhprzytocki@gmail.com>
2023-03-20 18:14:09 -0300

    feat: adding functions examples

--- mathematical_topics/functions/special_functions.r
+++
@@ -1,5 +0,0 @@
-sigmoid_function <- function(x){
-    e <- 2.71
-    y <- 1 / (1 + e^(-x))
-    return(y)
-}
--- mathematical_topics/parametric_functions/parametric_functions.r
+++
@@ -1,4 +0,0 @@
-linear_parametric_function <- function(x, theta){
-    y <- (x - theta)^2
-    return(y)
-}
